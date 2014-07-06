4818cfea8893477d23d91eb43cffe3669ec11bcc
Richard King <rkingucla@ymail.com>
2014-07-06 16:09:55 -0700

    update home js

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,66 +1,61 @@
-var map;
-var markers = [];
-var availableTags;
+$(function(){
+  var map;
+  var markers = [];
+  var SF_LAT = 37.7577;
+  var SF_LNG = -122.4376;
 
-$(document).on("submit", "form", function() {
-  searchMovie();
-  return false;
-});
-
-$(document).on("click", "#submit", function () {
-  searchMovie();
-});
-
-function searchMovie() {
-  deleteMarkers();
-
-  $.get("/api/movies/"+$("#movie").val(), function( data ) {
-    var locations_ary = data.locations;
-    var center_coords = new google.maps.LatLng(data.center[0], data.center[1]);
-    map.panTo(center_coords);
-    for ( var i = 0; i < locations_ary.length; i++ ) {
-      var myLatlng = new google.maps.LatLng(locations_ary[i].coordinates[0], locations_ary[i].coordinates[1]);
-      addMarker(myLatlng, locations_ary[i].name);
+  function deleteMarkers() {
+    for (var i = 0; i < markers.length; i++) {
+      markers[i].setMap(null);
     }
-  })
-}
-
-function setAllMap(map) {
-  for ( i = 0; i < markers.length; i++ ) {
-    markers[i].setMap(map);
+    markers = [];
   }
-}
 
-function addMarker(location, title) {
-  var marker = new google.maps.Marker({
-    position: location,
-      map: map,
-      title: title
-  });
-  markers.push(marker);
-}
-
-function clearMarkers() {
-  setAllMap(null)
-}
-
-function deleteMarkers() {
-  clearMarkers();
-  markers = [];
-}
-
-function initialize() {
-  $.get("/api/movies", function( data ) {
-    availableTags = data
-    $( "#movie" ).autocomplete({
-      source: availableTags
+  function addMarker(location, title) {
+    var marker = new google.maps.Marker({
+      position: location,
+        map: map,
+        title: title
     });
-  })
-  var mapOptions = {
-    center: new google.maps.LatLng(37.7577, -122.4376),
-    zoom: 12
-  };
-  map = new google.maps.Map(document.getElementById("map-canvas"),
-      mapOptions);
-}
-google.maps.event.addDomListener(window, "load", initialize);
+    markers.push(marker);
+  }
+
+  function searchMovie() {
+    deleteMarkers();
+
+    $.get("/api/movies/" + $("#movie").val(), function (data) {
+      var locations_ary = data.locations;
+      var center_coords = new google.maps.LatLng(data.center[0], data.center[1]);
+      var myLatlng;
+      map.panTo(center_coords);
+      for (var i = 0; i < locations_ary.length; i++) {
+        myLatlng = new google.maps.LatLng(locations_ary[i].coordinates[0], locations_ary[i].coordinates[1]);
+        addMarker(myLatlng, locations_ary[i].name);
+      }
+    });
+  }
+
+  function initialize() {
+    $.get("/api/movies", function( movieTitles ) {
+      $( "#movie" ).autocomplete({
+        source: movieTitles
+      });
+    });
+    var mapOptions = {
+      center: new google.maps.LatLng(SF_LAT, SF_LNG),
+      zoom: 12
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"),
+        mapOptions);
+
+    $(document).on("submit", "form", function() {
+      searchMovie();
+      return false;
+    });
+
+    $(document).on("click", "#submit", function () {
+      searchMovie();
+    });
+  }
+  google.maps.event.addDomListener(window, "load", initialize);
+});
--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -7,11 +7,10 @@ class Api::MoviesController < ApplicationController
 
   def show
     @movie = Movie.find_by_title(params[:title])
-    json = if @movie
-             @movie.build_location_json
-           else
-             { :message => "cannot find movie by that title" }
-           end
-    render :json => json
+    if @movie
+      render :json => @movie.build_location_json
+    else
+      render :json => { :status => "404", :error => "cannot find movie by that title" }, :status => 404
+    end
   end
 end
